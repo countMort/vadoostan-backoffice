c59f1dd56f9fecc0531440718eac709af7f0cb64
Morty <gholamimorty@gmail.com>
2025-10-04 04:23:03 +0330

    chore: ignore build errors

--- next.config.ts
+++ next.config.ts
@@ -4,7 +4,7 @@ import type { NextConfig } from "next"
 const nextConfig: NextConfig = {
   /* config options here */
   eslint: {
-    // ignoreDuringBuilds: true,
+    ignoreDuringBuilds: true,
   },
   images: {
     remotePatterns: [
