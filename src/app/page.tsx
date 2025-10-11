42f8409218449bdb6b2c31895a7098a30ade5d0f
Morty <gholamimorty@gmail.com>
2025-10-11 15:34:56 +0330

    test: telegram

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -36,7 +36,7 @@ export default function Home() {
 
   return (
     <div className="p-4 text-center h-[100vh] flex items-center justify-center flex-col">
-      <div>اطلاعات تلگرامی: {JSON.stringify(window.Telegram?.WebApp)}</div>
+      <div>اطلاعات تلگرامی: {JSON.stringify(window?.Telegram?.WebApp)}</div>
       {isInitializing && <Loading />}
       <Button component={Link} href={experience_create_route}>
         تجربه ها
